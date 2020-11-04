ca66ed6a6f7dac1121c5a19bc569400145ecb6fe
chohan3036 <chohan3036@gmail.com>
2020-11-04 17:57:42 +0900

    10025 solved

--- Binary Search/5639_이진 검색 트리.py
+++ Binary Search/5639_이진 검색 트리.py
@@ -5,22 +5,22 @@ read = lambda: sys.stdin.readline().strip()
 class Node(object):
     def __init__(self, data):
         self.data = data
-        self.left = Node(None)
-        self.right = Node(None)
+        self.left = None
+        self.right = None
 
 
 class BinarySearchTree(object):
     def __init__(self):
-        self.root = Node(None)
+        self.root = None
 
     def insert(self, node):
-        if not self.root.data:
-            self.root.data = node
+        if not self.root:
+            self.root = Node(node)
 
         parent = self.root
         # 루트보다 작을 경우
         while node < parent.data:
-            if parent.left.data:
+            if parent.left:
                 parent = parent.left
             else:
                 parent.left = Node(node)
@@ -28,22 +28,33 @@ class BinarySearchTree(object):
 
         # 루트보다 클 경우
         while node > parent.data:
-            if parent.right.data:
+            if parent.right:
                 parent = parent.right
             else:
                 parent.right = Node(node)
                 break
 
+    def search(self, node):
+        result = []
+        while True:
+            while node:
+                if node.right:
+                    result.append(node.right)
+                result.append(node)
+                node = node.left
+            node = result.pop()
+
+
 
 nodes = []
 while True:
-    cur_node = read()
-    if not cur_node:
+    input_node = read()
+    if not input_node:
         break
-    nodes.append(int(cur_node))
+    nodes.append(int(input_node))
 
 bst = BinarySearchTree()
 for node in nodes:
     bst.insert(node)
 
-print(bst)
+bst.search(bst.root)
